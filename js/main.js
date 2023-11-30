2aa35c1a0325ae437af27896edc2ce0179557301
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-11-30 18:29:03 +0100

    portoflio updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // document on content loaded
 document.addEventListener('DOMContentLoaded', () => {
+  // animate logo
+  animateLogo();
+
   const navLinks = document.querySelector('header nav .links');
   const links = navLinks.children;
   console.log(links);
@@ -11,7 +14,28 @@ document.addEventListener('DOMContentLoaded', () => {
       links[i].classList.add('opened');
     });
   }
+  // hamburger menu on click
+  const burgerMenu = document.querySelector('header nav .burger');
+  burgerMenu.addEventListener('click', function () {
+    if (burgerMenu.classList.contains('active')) {
+      burgerMenu.classList.remove('active');
+      navLinks.classList.remove('active');
+    } else {
+      burgerMenu.classList.add('active');
+      navLinks.classList.add('active');
+    }
+  });
 });
+
+function animateLogo() {
+  const logo = document.querySelector('header .logo');
+  let i = 0;
+  const inter = setInterval(() => {
+    logo.textContent = `0X${i}D`;
+    i++;
+    if (i == 5) clearInterval(inter);
+  }, 500);
+}
 
 function removeClass(list, className) {
   for (let i = 0; i < list.length; i++) {
