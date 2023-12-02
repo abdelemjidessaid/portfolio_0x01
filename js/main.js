12591104b719b102644bfea5935ac419cd8c6da8
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-12-02 17:35:05 +0100

    menu updated

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
   // animate logo
   animateLogo();
 
+  const darkMode = document.querySelector('header nav .links .mode');
+  const downlaodCv = document.querySelector('header .content .btn');
+  const sendButton = document.querySelector('.contact .send');
+  const body = document.querySelector('body');
+  const burgerMenu = document.querySelector('header nav .burger');
   const navLinks = document.querySelector('header nav .links');
   const links = navLinks.children;
 
@@ -14,39 +19,49 @@ document.addEventListener('DOMContentLoaded', () => {
     links[i].addEventListener('click', () => {
       removeClass(links, 'opened');
       links[i].classList.add('opened');
+      body.classList.remove('no-scroll');
+      burgerMenu.classList.remove('active');
+      navLinks.classList.remove('active');
     });
   }
 
   // dark mode button on click
-  const darkMode = document.querySelector('header nav .links .mode');
-  const body = document.querySelector('body');
   darkMode.addEventListener('click', function () {
-    body.classList.toggle('dark-mode');
+    const mode = localStorage.getItem('dark-mode');
+    if (mode === 'dark') {
+      localStorage.setItem('dark-mode', null);
+      body.classList.remove('dark-mode');
+    } else {
+      localStorage.setItem('dark-mode', 'dark');
+      body.classList.add('dark-mode');
+    }
   });
 
   // hamburger menu on click
-  const burgerMenu = document.querySelector('header nav .burger');
   burgerMenu.addEventListener('click', function () {
     if (burgerMenu.classList.contains('active')) {
       burgerMenu.classList.remove('active');
       navLinks.classList.remove('active');
+      body.classList.remove('no-scroll');
     } else {
       burgerMenu.classList.add('active');
       navLinks.classList.add('active');
+      body.classList.add('no-scroll');
     }
   });
 
   // download cs button on click
-  const downlaodCv = document.querySelector('header .content .btn');
   downlaodCv.addEventListener('click', function () {
     showAlert();
   });
 
   // button of send on click
-  const sendButton = document.querySelector('.contact .send');
   sendButton.addEventListener('click', function () {
     showAlert();
   });
+
+  // check if was dark mode applied
+  checkDarkMode();
 });
 
 function animateLogo() {
@@ -67,4 +82,13 @@ function removeClass(list, className) {
 
 function showAlert() {
   alert('This functionality does not work yet');
+}
+
+function checkDarkMode() {
+  const value = localStorage.getItem('dark-mode');
+  const body = document.querySelector('body');
+  console.log(value);
+  if (value === 'dark') {
+    body.classList.add('dark-mode');
+  }
 }
