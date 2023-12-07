ebb472df7d71e9fc734b837ecc9b461836e3c8f7
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-12-07 17:59:14 +0100

    some comments added to the JavaScript code

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // animate logo
   animateLogo();
 
+  // initialize all constants
   const darkMode = document.querySelector('header nav .links .mode');
   const downlaodCv = document.querySelector('header .content .btn');
   const sendButton = document.querySelector('.contact .send');
@@ -13,9 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // on a nav link clicked
   for (let i = 0; i < links.length; i++) {
+    // prevent the dark mode button from containing the 'open' class
     if (links[i].classList.contains('mode')) {
       continue;
     }
+    // every link should has a click event listener
     links[i].addEventListener('click', () => {
       removeClass(links, 'opened');
       links[i].classList.add('opened');
@@ -65,6 +68,9 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function animateLogo() {
+  /**
+   * animateLogo - function that does animation to the logo
+   */
   const logo = document.querySelector('header .logo');
   let i = 0;
   const inter = setInterval(() => {
@@ -75,19 +81,33 @@ function animateLogo() {
 }
 
 function removeClass(list, className) {
+  /**
+   * removeClass - function that removes class name 'open' from
+   * all elements of the given list.
+   * @list: list of html elements
+   * @className: the class name
+   */
   for (let i = 0; i < list.length; i++) {
     list[i].classList.remove(className);
   }
 }
 
 function showAlert() {
+  /**
+   * showAlert - function that displays an alert for buttons that
+   * does not have functionality.
+   */
   alert('This functionality does not work yet');
 }
 
 function checkDarkMode() {
+  /**
+   * checkDarkMode - function that check the last value of dark mode
+   * saved on the loacal storage, to apply the dark mode or not automatically
+   * when the user visited the website another time, or refreshed the page.
+   */
   const value = localStorage.getItem('dark-mode');
   const body = document.querySelector('body');
-  console.log(value);
   if (value === 'dark') {
     body.classList.add('dark-mode');
   }
